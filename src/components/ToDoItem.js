c71ba1a969ccea0478c954587b48f4faf4d79d22
Your Name~ <hmphtetminpaing@gmail.com>
2024-01-11 21:40:21 +0630

    Change the TODoItem.js

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { format } from 'date-fns/esm';
+import { MdDelete, MdEdit } from 'react-icons/md';
 import style from '../styles/modules/todoItem.module.scss';
 import { getClasses } from '../utils/getClasses';
 
 function ToDoItem({ todo }) {
+  const handleDelete = () => {
+    console.log('Delete');
+  };
+
+  const handleEdit = () => {
+    console.log('Edit');
+  };
   return (
     <div className={style.item}>
       <div className={style.todoDetails}>
@@ -15,6 +24,31 @@ function ToDoItem({ todo }) {
           >
             {todo.title}
           </p>
+          <p className={style.time}>
+            {format(new Date(todo.time), 'p, MM/dd/yyyy')}
+          </p>
+        </div>
+      </div>
+      <div className={style.todoActions}>
+        <div
+          className={style.icon}
+          role="button"
+          tabIndex={0}
+          onClick={handleDelete}
+          onKeyDown={handleDelete}
+          aria-label="Delete"
+        >
+          <MdDelete />
+        </div>
+        <div
+          className={style.icon}
+          role="button"
+          tabIndex={0}
+          onClick={handleEdit}
+          onKeyDown={handleEdit}
+          aria-label="Edit"
+        >
+          <MdEdit />
         </div>
       </div>
     </div>
